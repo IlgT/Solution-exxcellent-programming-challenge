337dc62988de5e919cc57710deb139a70d65dca7
Tobias Ilg <tobias.ilg@gmx.net>
2018-02-09 16:15:24 +0100

    Implementation of class App

--- src/main/java/de/exxcellent/challenge/App.java
+++ src/main/java/de/exxcellent/challenge/App.java
@@ -11,13 +11,44 @@ import java.util.List;
  */
 public final class App {
     /**
-     * Main method that outputs the result of calculations in the console.
+     * The main method,
      *
      * @param args Command line parameters
      *
      * @author Tobias Ilg <tobias.ilg@gmx.net>
      */
     public static void main(String... args) {
-//        Todo: Implement
+
+        String weatherFilePath = "src/main/resources/de/exxcellent/challenge/weather.csv";
+        List<String> daysWithSmallestTempSpread = Calculations.getElementsByDifference(weatherFilePath,"Day",
+                "MxT", "MnT");
+        String footballFilePath = "src/main/resources/de/exxcellent/challenge/football.csv";
+        List<String> teamsWithSmallestGoalSpread = Calculations.getElementsByDifference(footballFilePath,"Team",
+                "Goals", "Goals Allowed");
+
+
+        int counter = 0;
+        for (String day: daysWithSmallestTempSpread) {
+            if (counter == 0) {
+                System.out.printf("Day(s) with smallest temperature spread : %s.", day);
+                counter++;
+            } else {
+                System.out.printf(" and %s.", day);
+                counter++;
+            }
+        }
+        System.out.printf("%n");
+
+        counter = 0;
+        for (String team: teamsWithSmallestGoalSpread) {
+            if (counter == 0) {
+                System.out.printf("Team(s) with smallest goal spread       : %s", team);
+                counter++;
+            } else {
+                System.out.printf(" and %s", team);
+                counter++;
+            }
+        }
+        System.out.printf("%n");
     }
 }
